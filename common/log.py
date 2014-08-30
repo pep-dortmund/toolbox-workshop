eb3f89f6fe9f5d406419826491ca5f734e1fb6db
Ismo Toijala <ismo.toijala@gmail.com>
2014-08-30 18:42:41 +0200

    common: log: complete rewrite, now with more features and understandable code!

--- common/log.py
+++ common/log.py
@@ -6,233 +6,243 @@ import sys
 
 logfile = open(sys.argv[1], "r")
 lines = [line.rstrip("\n") for line in logfile.readlines()]
+lines.reverse()
 
-re_file_page  = re.compile(r'^ ?((\)|\(([^ ")]+|"[^"]+")|\[[0-9]*|\]|<[^>]*>|\{[^}]*\}|\((using write cache|using read cache|load luc): [^)]*\)|ABD: EveryShipout initializing macros|luatex-hyphen: using data file: ([^ ")]+|"[^"]+")|luatex-hyphen: info: no hyphenation exceptions for this language|at position [0-9]+ in \'luaotfload.patch_font\'|luaotfload \| db : Font names database loaded from [^(]+|luaotfload \| load : Lookup/name: [^(]+) ?)+$')
-re_file_page_interesting = re.compile(r'\)|\((?:[^ ")]+|"[^"]+")|\[[0-9]+|\]')
+def parse_path_page(lines):
+    global paths, pages, page
+    parts = [
+            r'\)',
+            r'\(([^ ")]+|"[^"]+")',
+            r'\[[0-9]*',
+            r'\]',
+            r'<[^>]*>',
+            r'|\{[^}]*\}',
+            r'\((using write cache|using read cache|load luc): [^)]*\)',
+            r'ABD: EveryShipout initializing macros',
+            r'luatex-hyphen: using data file: ([^ ")]+|"[^"]+")',
+            r'luatex-hyphen: info: no hyphenation exceptions for this language',
+            r'at position [0-9]+ in \'luaotfload.patch_font\'',
+            r'luaotfload \| db : Font names database loaded from [^(]+',
+            r'luaotfload \| load : Lookup/name: [^(]+',
+            ]
+    if not re.match(r'^ ?(({}) ?)+$'.format('|'.join(parts)), lines[-1]):
+        return None
+    line = lines.pop()
+    for match in re.findall(r'\)|\((?:[^ ")]+|"[^"]+")|\[[0-9]+|\]', line):
+        if match == ")":# and len(files) > 1: # should not happen
+            paths.pop()
+        elif match[0] == "(":
+            paths.append(match[1:].strip('"'))
+        elif match == "]":
+            if page is not None: # this can happen: there are also empty []
+                pages.append(int(page))
+            page = None
+        elif match[0] == "[":
+            page = match[1:]
+    return False
 
-re_latex_error = re.compile(r'^! ')
-re_latex_error_line = re.compile(r'^l\.([0-9]+) ')
+def parse_latex2_error(lines):
+    match = re.match(r'^! (?:LaTeX|Class|Package) (?:([^ ]+) )?Error: ', lines[-1])
+    if not match:
+        return None
+    message = [lines.pop()]
+    empties = 0
+    while empties < 2:
+        line = lines.pop()
+        if not line:
+            empties += 1
+        if line.rstrip() == " ...":
+            message.pop() # See the first-argument package documentation for explanation.
+            message.pop() # Type  H <return>  for immediate help.
+            message.pop() # empty
+        else:
+            message.append(line)
+    message.pop()
+    return {
+            "type": "latex2/error",
+            "message": message,
+            }
 
-re_latex_warning = re.compile(r'^LaTeX Warning: ')
-re_latex_warning_undefined_reference = re.compile(r"^LaTeX Warning: Reference `([^']+)' on page [0-9]+ undefined on input line ([0-9]+)\.$")
-re_latex_warning_undefined_references = r'LaTeX Warning: There were undefined references.'
-re_latex_warning_multiple_label = re.compile(r"^LaTeX Warning: Label `([^']+)' multiply defined\.$")
-re_latex_warning_multiple_labels = r'LaTeX Warning: There were multiply-defined labels.'
-re_latex_warning_rerun = r'LaTeX Warning: Label(s) may have changed. Rerun to get cross-references right.'
+def parse_latex2(lines):
+    match = re.match(r'^(?:LaTeX|Class|Package) (?:([^ ]+) )?(Warning|Info): ', lines[-1])
+    if not match:
+        return None
+    if match.groups()[0]:
+        continuation = "({})".format(match.groups()[0])
+    else:
+        continuation = " " * len(match.string[match.start():match.end()]) # LaTeX (Warning|Info)
+    message = [lines.pop()]
+    while lines[-1].startswith(continuation):
+        message.append(lines.pop())
+    return {
+            "type": "latex2/" + match.groups()[1].lower(),
+            "message": message,
+            }
 
-re_latex_info = re.compile(r'^LaTeX ([^ ]+ )?Info: ')
+def parse_latex3_error(lines):
+    if lines[-1] != r'!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!':
+        return None
+    lines.pop()
+    lines.pop()
+    message = []
+    while lines[-1]:
+        line = lines.pop()
+        if line.rstrip() == "!...............................................":
+            message.pop() # For immediate help type H <return>.
+            message.pop() # empty
+            continue
+        if line in [
+                "|'''''''''''''''''''''''''''''''''''''''''''''''",
+                "|..............................................."
+                ]:
+            continue
+        if line[:2] in ["! ", "| "]:
+            line = line[2:]
+        message.append(line)
+    return {
+            "type": "latex3/error",
+            "message": message,
+            }
 
-re_package_error = re.compile(r'^! Package ([^ ]+) Error: ')
+def parse_latex3(lines):
+    types = {
+            r'*************************************************': "warning",
+            r'.................................................': "info",
+            }
+    if lines[-1] not in types:
+        return None
+    line = lines.pop()
+    message = []
+    while lines[-1] != line:
+        message.append(lines.pop()[2:])
+    lines.pop()
+    return {
+            "type": "latex3/" + types[line],
+            "message": message,
+            }
 
-re_package_warning = re.compile(r'^Package ([^ ]+) Warning: ')
-re_package_rerunfilecheck_warning_rerun = re.compile(r"^Package rerunfilecheck Warning: File `[^']+' has changed\.( Rerun\.)?$")
-re_package_biblatex_warning_rerun = re.compile('^Package biblatex Warning: Please \(re\)run Biber on the file:$')
+def parse_tex_error(lines):
+    if not re.match(r'^! ', lines[-1]):
+        return None
+    message = [lines.pop()]
+    while lines and lines[-1]:
+        message.append(lines.pop())
+    return {
+            "type": "tex/error",
+            "message": message,
+            }
 
-re_package_info = re.compile(r'^Package ([^ ]+) Info: ')
+def parse_box(lines):
+    if not re.match(r'^(?:Over|Under)full \\(?:h|v)box', lines[-1]):
+        return None
+    message = [lines.pop()]
+    if "paragraph" in message[0]:
+        message.append(lines.pop())
+    return {
+            "type": "box",
+            "message": message,
+            }
 
-re_latex3_error = r'!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-re_latex3_error_end = r'|...............................................'
-re_latex3_error_print_ignore = [
-        r'! For immediate help type H <return>.',
-        r'!...............................................',
-        r"|'''''''''''''''''''''''''''''''''''''''''''''''",
-        ]
-
-re_latex3_warning = r'*************************************************'
-re_latex3_warning_ignore = [
-        r'LaTeX warning: "xparse/redefine-command"',
-        r'LaTeX warning: "xparse/redefine-environment"',
-        r'unicode-math warning: "mathtools-overbracket"',
-        ]
-
-re_latex3_info = r'.................................................'
-
-re_full_box = re.compile(r'^(?:Over|Under)full \\(?:h|v)box')
-
-files = list()
+paths = []
 pages = [0]
 page = None
+messages = []
+
+parsers = [
+        parse_path_page,
+        parse_latex2_error,
+        parse_latex2,
+        parse_latex3_error,
+        parse_latex3,
+        parse_tex_error,
+        parse_box,
+        ]
+
+while lines:
+    for parser in parsers:
+        message = parser(lines)
+        if message:
+            message["path"] = paths[-1] if len(paths) else None
+            message["page"] = pages[-1] + 1
+            messages.append(message)
+        if message is not None:
+            break
+    else:
+        lines.pop()
+
+def filter_info(message):
+    if message["type"].endswith("/info"):
+        return None
+    return message
+
+def filter_unicode_math_mathtools(message):
+    if message["type"] == "latex3/warning" and message["message"][0] in [
+            r'unicode-math warning: "mathtools-colon"',
+            r'unicode-math warning: "mathtools-overbracket"',
+            ]:
+        return None
+    return message
+
+def filter_biblatex_footnotes(message):
+    if message["type"] == "latex2/warning" and message["message"][0] == r'Package biblatex Warning: Patching footnotes failed.':
+        return None
+    return message
+
+def filter_latex2_rerun_tex(message):
+    global exit_code
+    warnings = [
+            r'^LaTeX Warning: Label\(s\) may have changed\. Rerun to get cross-references right\.$',
+            r"^Package rerunfilecheck Warning: File `[^']+' has changed\.( Rerun\.)?$",
+            r'^Package biblatex Warning: Please rerun LaTeX\.$',
+            ]
+    if message["type"] == "latex2/warning" and any(re.match(warning, message["message"][0]) for warning in warnings):
+        exit_code |= 2
+    return message
+
+def filter_biblatex_rerun_biber(message):
+    global exit_code
+    if message["type"] == "latex2/warning" and message["message"][0] == r'Package biblatex Warning: Please (re)run Biber on the file:':
+        exit_code |= 4
+    return message
+
+exit_code = 0
+
+filters = [
+        filter_info,
+        filter_unicode_math_mathtools,
+        filter_biblatex_footnotes,
+        filter_latex2_rerun_tex,
+        filter_biblatex_rerun_biber,
+        ]
+
+messages_copy = list(messages)
+messages = []
+for message in messages_copy:
+    for filt in filters:
+        message = filt(message)
+        if not message:
+            break
+    else:
+        messages.append(message)
 
 def relativise_path(path):
+    if path.startswith('./'):
+        path = path[2:]
     if not os.path.isabs(path):
         return path
     rel = os.path.relpath(os.path.abspath(path))
     return path if "../.." in rel else rel
 
-def print_file_page(line=None):
-    if line is None:
-        print("{} (page {}):".format(relativise_path(files[-1]), pages[-1] + 1))
+for message in messages:
+    if message["path"] is not None:
+        print(relativise_path(message["path"]), end="")
+        if message["page"] is not None:
+            print(" (page {})".format(message["page"]), end="")
+        print(":")
+        indent = "  "
     else:
-        print("{} (line {}, page {}):".format(relativise_path(files[-1]), line, pages[-1] + 1))
-
-def print_latex_error(error):
-    for line in error:
-        print("  " + line)
-
-latex_error = None
-
-def print_package(package):
-    for line in package[1]:
-        print("  " + line)
-
-package_error = None
-package_warning = None
-package_info = None
-
-def print_latex3(latex3):
-    for line in latex3[1:-1]:
-        print("  " + line[2:])
-
-def print_latex3_error(error):
-    for line in error[2:-1]:
-        if line not in re_latex3_error_print_ignore and line:
-            if line[:2] == "l.":
-                print("  " + line)
-                print()
-            else:
-                print("  " + line[2:])
-
-latex3_error = False
-latex3_warning = False
-latex3_info = False
-
-exit_code = 0
-
-for lineno, line in enumerate(lines):
-    line = line.rstrip()
-    if latex3_info is not False:
-        latex3_info.append(line)
-        if line == re_latex3_info:
-            #print_file_page()
-            #print_latex3(latex3_info)
-            latex3_info = False
-    elif latex3_warning is not False:
-        latex3_warning.append(line)
-        if line == re_latex3_warning:
-            if latex3_warning[1][2:] not in re_latex3_warning_ignore:
-                print_file_page()
-                print_latex3(latex3_warning)
-            latex3_warning = False
-    elif latex3_error is not False:
-        latex3_error.append(line)
-        if line == re_latex3_error_end:
-            print_file_page()
-            print_latex3_error(latex3_error)
-            latex3_error = False
-    elif package_error is not None and line.startswith(r'({}) '.format(package_error[0])):
-        package_error[1].append(line)
-    elif package_warning is not None and line.startswith(r'({}) '.format(package_warning[0])):
-        package_warning[1].append(line)
-        if not lines[lineno + 1].startswith(r'({}) '.format(package_warning[0])):
-            print_file_page()
-            print_package(package_warning)
-            package_warning = None
-    elif package_info is not None and line.startswith(r'({}) '.format(package_info[0])):
-        package_info[1].append(line)
-        if not lines[lineno + 1].startswith(r'({}) '.format(package_info[0])):
-            #print_file_page()
-            #print_package(package_info)
-            package_info = None
-    elif re_file_page.fullmatch(line) and re_file_page_interesting.findall(line):
-        matches = re_file_page_interesting.findall(line)
-        for match in matches:
-            if match == ")" and len(files) > 1:
-                files.pop()
-            elif match[0] == "(":
-                files.append(match[1:].strip('"'))
-            elif match == "]":
-                if page is not None:
-                    pages.append(int(page))
-                page = None
-            elif match[0] == "[":
-                page = match[1:]
-    elif re_package_error.match(line):
-        match = re_package_error.match(line)
-        package_error = match.groups()[0], [line], []
-    elif re_latex_error.match(line):
-        latex_error = [line]
-        if lineno == len(lines) - 1:
-            print_file_page()
-            print("  " + line)
-    elif line == re_latex_warning_undefined_references or re_latex_warning_multiple_label.match(line) or line == re_latex_warning_multiple_labels:
-        print(line)
-    elif line == re_latex_warning_rerun:
-        exit_code |= 2
-        print(line)
-    elif re_latex_warning_undefined_reference.match(line):
-        match = re_latex_warning_undefined_reference.match(line)
-        print_file_page(match.groups()[1])
-        print("  LaTeX Warning: Reference `{}' undefined".format(match.groups()[0]))
-    elif re_latex_warning.match(line):# or re_latex_info.match(line):
-        print_file_page()
-        print("  " + line)
-    elif re_package_rerunfilecheck_warning_rerun.match(line):
-        exit_code |= 2
-        match = re_package_warning.match(line)
-        package_warning = match.groups()[0], [line]
-        if not lines[lineno + 1].startswith(r'({}) '.format(package_warning[0])):
-            print_file_page()
-            print("  " + line)
-            package_warning = None
-    elif re_package_biblatex_warning_rerun.match(line):
-        exit_code |= 4
-        match = re_package_warning.match(line)
-        package_warning = match.groups()[0], [line]
-        if not lines[lineno + 1].startswith(r'({}) '.format(package_warning[0])):
-            print_file_page()
-            print("  " + line)
-            package_warning = None
-    elif re_package_warning.match(line):
-        match = re_package_warning.match(line)
-        package_warning = match.groups()[0], [line]
-        if not lines[lineno + 1].startswith(r'({}) '.format(package_warning[0])):
-            print_file_page()
-            print("  " + line)
-            package_warning = None
-    elif re_package_info.match(line):
-        match = re_package_info.match(line)
-        package_info = match.groups()[0], [line]
-        if not lines[lineno + 1].startswith(r'({}) '.format(package_info[0])):
-            #print_file_page()
-            #print("  " + line)
-            package_info = None
-    elif re_full_box.match(line):
-        print_file_page()
-        print("  " + line)
-        if "paragraph" in line:
-            print("  " + lines[lineno + 1])
-    elif line == re_latex3_info:
-        latex3_info = [line]
-    elif line == re_latex3_warning:
-        latex3_warning = [line]
-    elif line == re_latex3_error:
-        latex3_error = [line]
-    elif latex_error is not None:
-        if re_latex_error_line.match(line):
-            latex_error.append(line)
-            match = re_latex_error_line.match(line)
-            print_file_page(match.groups()[0])
-            print_latex_error(latex_error)
-            latex_error = None
-        elif len(latex_error) < 7 and lineno < len(lines) - 1:
-            latex_error.append(line)
-        else:
-            print_file_page()
-            print("  " + latex_error[0])
-            latex_error = None
-    elif package_error is not None:
-        if re_latex_error_line.match(line):
-            package_error[2].append(line)
-            match = re_latex_error_line.match(line)
-            print_file_page(match.groups()[0])
-            print_latex_error(package_error[1])
-            print_latex_error(package_error[2])
-            package_error = None
-        elif len(package_error[2]) < 7 and lineno < len(lines) - 1:
-            package_error[2].append(line)
-        else:
-            print_file_page()
-            print_latex_error(package_error[1])
-            package_error = None
+        indent = ""
+    for line in message["message"]:
+        print(indent, line)
 
 sys.exit(exit_code)
