96556c6ad0eb5466a41c207b1271f95022ad004d
Maximilian Noethe <maximilian.noethe@udo.edu>
2014-09-20 19:15:48 +0200

    modified motivation plot

--- python/muon_plot.py
+++ python/muon_plot.py
@@ -2,17 +2,29 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
 from uncertainties import ufloat, correlated_values
+from matplotlib.patches import Rectangle
+import uncertainties.unumpy as unp
+
+nom = unp.nominal_values
+std = unp.std_devs
+
+plt.rcParams["text.usetex"] = True
+plt.rcParams["legend.fontsize"] = plt.rcParams["font.size"]
+
 
 
 def decay_with_background(t, N0, lamb, back):
     return N0 * np.exp(-lamb*t) + back
 
+def u_decay_with_background(t, N0, lamb, back):
+    return N0 * unp.exp(-lamb*t) + back
+
 N = np.genfromtxt("./muon_data.txt")
-N = np.delete(N, [0,1,2,3, -1, -2, -3, -4, -5, -6])
-t = (np.arange(len(N)) + 5)/21.48
+N = np.delete(N, [0,1,2,3, len(N)-1, len(N)-2, len(N)-3, len(N)-4, len(N)-5])
+N = N[::2]
+t = (2*np.arange(len(N)) + 5)/21.48
 N_err = np.sqrt(N)
 
-t_plot = np.linspace(0, max(t), 1000)
 
 params, cov = curve_fit(decay_with_background, t, N, p0=[400, 1, 5])
 
@@ -21,18 +33,35 @@ N0, Lamb, U = correlated_values(params, cov)
 print("N0=", N0)
 print("λ=", Lamb)
 print("U=", U)
-
 print("τ=", 1/Lamb)
 
+t_plot = np.linspace(0, max(t), 1000)
+y_plot = u_decay_with_background(t_plot, N0, Lamb, U)
 
 fig = plt.figure(figsize=(15/2.54, 10/2.54))
 ax = fig.add_subplot(1,1,1)
 
-ax.errorbar(t, N, yerr=N_err, fmt='k+', label="Messwerte")
-ax.plot(t_plot, decay_with_background(t_plot, *params), 'r-', label="Fit")
+data = ax.errorbar(t, N, yerr=N_err, fmt='k+', label="Messwerte", capsize=1, ms=3, capthick=0.5, elinewidth=0.75)
+fit, = ax.plot(t_plot, nom(y_plot), 'r-', label="Fit", lw=0.5)
+ax.fill_between(t_plot, nom(y_plot)-2*std(y_plot), nom(y_plot)+2*std(y_plot),
+                color='r', alpha=0.3, lw=0)
 
-ax.legend(loc="best")
-ax.set_xlabel(r"$t/\si{\micro\second}$")
+ax.set_title("Plot: matplotlib")
+
+ax.annotate("Messwerte einlesen: numpy", xy=(t[9],N[9]),
+            xytext=(t[9]+0.5, N[9]+10))
+
+ax.annotate("Fit: scipy", xy=(t[50],N[50]),
+            xytext=(t[50]+0.5, N[50]+30), color='r')
+
+ax.annotate("Fehlerrechnung: uncertainties", xy=(t[20],N[20]),
+            xytext=(t[20]+0.5, N[20]+30), color='r', alpha=0.3)
+
+proxy = Rectangle((0,0),0,0, color='r', alpha=0.3, linewidth=0)
+
+ax.legend([data, fit, proxy], ["Messwerte", "Fit", "$1\sigma$-Umgebung"], loc="best")
+ax.set_xlabel(r"$t/\upmu$s")
 ax.set_ylabel(r"Zählrate")
+ax.set_xlim(0,11)
 fig.tight_layout()
-fig.savefig("muon_plot.png", dpi=100)
+fig.savefig("muon_plot.png", dpi=150)
