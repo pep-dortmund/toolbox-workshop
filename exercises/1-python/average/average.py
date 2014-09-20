d75b8a7ca6c9455d1d1788f1e520e7a85282a498
Igor Babuschkin <igor@babuschk.in>
2014-09-21 01:25:22 +0200

    Add Python exercise: average

---
+++ exercises/1-python/average/average.py
@@ -0,0 +1,6 @@
+
+data = [42, 3.1415, 2.7182, 1, 2]
+
+# Hier den Mittelwert berechnen und ausgeben
+
+
---
+++ exercises/1-python/average/lösung.py
@@ -0,0 +1,7 @@
+
+data = [42, 3.1415, 2.7182, 1, 2]
+
+avg = sum(data) / len(data)
+
+print('Der Mittelwert ist ', avg)
+
