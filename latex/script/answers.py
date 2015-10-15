23f39ad1744304eed20939a564b3459e29247e62
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2015-10-15 22:48:21 +0200

    completly fix plot for latex

--- latex/script/answers.py
+++ latex/script/answers.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.style import use
 use('ggplot')
+plt.rcParams['font.family'] = 'sans-serif'
 
 colors = [
     '#D0583A',
@@ -16,8 +17,8 @@ answers = pd.read_csv('data/2015.tsv', sep='\t')
 os = answers['Betriebssystem'].value_counts()
 os /= os.sum()
 
-fig = plt.figure(figsize=(4, 3))
-ax = fig.add_axes([0.125, 0, 0.75, 1])
+fig = plt.figure()
+ax = fig.add_axes([0.125, 0, 0.75, 1], aspect=1)
 ax.pie(os.values, labels=os.keys(), colors=colors, startangle=-10)
 fig.savefig('build/figures/os.pdf')
 
@@ -26,7 +27,7 @@ experience = answers['Erfahrung mit LaTeX']
 experience = experience.value_counts()
 experience /= experience.sum()
 
-fig = plt.figure(figsize=(4, 3))
-ax = fig.add_axes([0.125, 0.0, 0.75, 1])
+fig = plt.figure()
+ax = fig.add_axes([0.125, 0.0, 0.75, 1], aspect=1)
 ax.pie(experience.values, labels=experience.keys(), colors=colors)
 fig.savefig('build/figures/experience.pdf')
