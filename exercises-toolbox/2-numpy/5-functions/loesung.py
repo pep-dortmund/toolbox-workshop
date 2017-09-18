ec6e9132857601bda356664123d741d8554c8596
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-09-18 09:24:12 +0200

    added disclaimer for ex 5

--- exercises-toolbox/2-numpy/5-functions/loesung.py
+++ exercises-toolbox/2-numpy/5-functions/loesung.py
@@ -1,11 +1,15 @@
 #! /usr/bin/env python
 # encoding: utf-8
 
+# Ganz wichtig: KEINE PANIK!
+# Diese Lösung ist etwas lang, das sollte dich aber nicht abschrecken, die meisten Zeilen
+# sind print-Funktionen. Es geht hier nicht darum, die Lösung 1 zu 1 nach zu arbeiten,
+# es geht nur darum, ein Gefühl dafür zu bekommen, wie die numpy Funktionen in unterschiedlichen
+# Dimensionen funktionieren.
+
 import numpy as np
 
-
 A = np.arange(5)
-
 
 print("Aufgabe 1)")
 print(f"Array A: {A}")
