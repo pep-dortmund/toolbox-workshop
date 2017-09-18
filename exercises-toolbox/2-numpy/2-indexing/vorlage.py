92faa0d6cf5917809c3d468cf2bb0c92e72730a8
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-09-18 09:36:12 +0200

    added 'ignore me' disclaimer to plot funcs in ex 2

--- exercises-toolbox/2-numpy/2-indexing/vorlage.py
+++ exercises-toolbox/2-numpy/2-indexing/vorlage.py
@@ -18,6 +18,8 @@ field = np.array([[4, 0, 1, 0, 0, 1, 2, 2, 0, 0],
 print("Spielfeld zu Beginn des Spiels:")
 print(field)
 
+# Was das hier ist lernen wir noch, kann getrost ignoriert werden.
+# In der Zwischenzeit, die Kurzfassung: Das 'macht' die Bilder ;-)
 plt.matshow(field, cmap="Set1", vmax=5, vmin=-1)
 plt.savefig("Spielfeld_Beginn.pdf")
 
@@ -30,5 +32,7 @@ field[0, 6:8] = -1
 print("Spielfeld zum Ende des Spiels:")
 print(field)
 
+# Was das hier ist lernen wir noch, kann getrost ignoriert werden.
+# In der Zwischenzeit, die Kurzfassung: Das 'macht' die Bilder ;-)
 plt.matshow(field, cmap="Set1", vmax=5, vmin=-1)
 plt.savefig("Spielfeld_Ende.pdf")
