9d8cc7696aa07f33a9e662d9b024cc75794b489e
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-09-18 09:29:07 +0200

    added template file for ex 5

---
+++ exercises-toolbox/2-numpy/5-functions/vorlage.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+# encoding: utf-8
+
+# Ganz wichtig: KEINE PANIK!
+# Diese Lösung ist etwas lang, das sollte dich aber nicht abschrecken, die meisten Zeilen
+# sind print-Funktionen. Es geht hier nicht darum, die Lösung 1 zu 1 nach zu arbeiten,
+# es geht nur darum, ein Gefühl dafür zu bekommen, wie die numpy Funktionen in unterschiedlichen
+# Dimensionen funktionieren.
+
+import numpy as np
+
+A = np.arange(5)
+
+# Lösung zu 1) hier:
+
+
+
+
+B = np.array([[2, 1, 2, 1, 2], [1, 2, 1, 2, 1], [3, 1, 3, 1, 3]])
+
+# Lösung zu 2) hier:
+
+
+
+# Lösung zu 3) hier:
