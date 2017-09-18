4653c89737faf676b33bd99e3a6889d4d0937a3f
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2017-09-18 09:01:40 +0200

    changed order of print in solution for ex 5

--- exercises-toolbox/2-numpy/5-functions/loesung.py
+++ exercises-toolbox/2-numpy/5-functions/loesung.py
@@ -71,11 +71,12 @@ print("Daten\n", data)
 sum_per_person = np.sum(data, axis=0)
 print("Summe pro Person:", sum_per_person)
 
-sum_data = np.sum(data)
-print("Gesamtsumme:", sum_data)
 
 mean_per_person = np.mean(data, axis=0)
 print("Mittel pro Person:", mean_per_person)
+
+sum_data = np.sum(data)
+print("Gesamtsumme:", sum_data)
 
 sum_per_day = np.sum(data, axis=1)
 print("Summe pro Tag:", sum_per_day)
