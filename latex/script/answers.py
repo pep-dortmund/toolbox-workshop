6c47184a308574f3b83f42d4dc3a08371d73ecea
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-09-20 17:24:35 +0200

    Update plots for 2017

--- latex/script/answers.py
+++ latex/script/answers.py
@@ -4,22 +4,21 @@ import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 plt.rcParams['font.family'] = 'sans-serif'
 
-colors = [
-    '#D0583A',
-    '#7777CD',
-    '#7FBC3B',
-    '#CC52B0',
-    'yellow',
-]
-
-answers = pd.read_csv('data/2016.csv')
+answers = pd.read_csv('data/2017.csv')
 
 os = answers['Betriebssystem'].value_counts()
 os /= os.sum()
 
-fig = plt.figure()
-ax = fig.add_axes([0.125, 0, 0.75, 1], aspect=1)
-ax.pie(os.values, labels=os.keys(), colors=colors, startangle=-10)
+fig = plt.figure(figsize=(5.3, 3.3))
+ax = fig.add_axes([0, 0, 1, 1], aspect=1)
+ax.pie(
+    os.values,
+    labels=os.index,
+    startangle=-10,
+    radius=1,
+    center=(0, 0),
+)
+ax.set_xlim(-1, 2)
 fig.savefig('build/figures/os.pdf')
 
 experience = answers['Erfahrung mit LaTeX']
@@ -27,7 +26,12 @@ experience = answers['Erfahrung mit LaTeX']
 experience = experience.value_counts()
 experience /= experience.sum()
 
-fig = plt.figure()
-ax = fig.add_axes([0.125, 0.0, 0.75, 1], aspect=1)
-ax.pie(experience.values, labels=experience.keys(), colors=colors)
+fig = plt.figure(figsize=(5.3, 3.3))
+ax = fig.add_axes([0, 0, 1, 1], aspect=1)
+ax.pie(
+    experience.values,
+    labels=experience.index,
+    radius=1,
+)
+ax.set_xlim(-1, 2)
 fig.savefig('build/figures/experience.pdf')
