eb20cbe8123cf85155f775431ff79a293ea212bf
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-09-28 23:22:13 +0200

    Use uncertainties in V406

--- exercises-toolbox/8-all/406_Beugung_am_Spalt/loesung.py
+++ exercises-toolbox/8-all/406_Beugung_am_Spalt/loesung.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
+from uncertainties import correlated_values
 
 
 def theory(phi, A0, b):
@@ -18,11 +19,15 @@ I *= 1e-9
 zeta_0 = zeta[np.argmax(I)]
 phi = (zeta - zeta_0) / L
 
-popt, pcov = curve_fit(theory, phi, I, p0=[np.sqrt(np.max(I)) / 1e-4, 1e-4])
-print(popt, np.sqrt(np.diag(pcov)), sep='\n')
+params, cov = curve_fit(theory, phi, I, p0=[np.sqrt(np.max(I)) / 1e-4, 1e-4])
+
+A0, b = correlated_values(params, cov)
+
+print('A0 =', A0)
+print('b =', b)
 
 x = np.linspace(-0.03, 0.03, 100)
-plt.plot(x, theory(x, *popt), label='Fit')
+plt.plot(x, theory(x, *params), label='Fit')
 plt.plot(phi, I, 'x', label='Daten')
 plt.xlabel(r'$\varphi \,\, / \,\, \mathrm{rad}$')
 plt.ylabel(r'$I \,\, / \,\, \mathrm{A}$')
