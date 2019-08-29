cc74351958131b2c886724ff4bb3f7d90c1148b2
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2019-08-29 11:32:18 +0200

    Clean up python directory

--- python/muon_plot.py
+++ python/muon_plot.py
@@ -1,33 +1,30 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
-from uncertainties import ufloat, correlated_values
+from uncertainties import correlated_values
 from matplotlib.patches import Rectangle
 import uncertainties.unumpy as unp
 
 nom = unp.nominal_values
 std = unp.std_devs
 
-plt.rcParams["text.usetex"] = True
-plt.rcParams["legend.fontsize"] = plt.rcParams["font.size"]
-
 
 def decay_with_background(t, N0, lamb, back):
-    return N0 * np.exp(-lamb*t) + back
+    return N0 * np.exp(-lamb * t) + back
 
 
 def u_decay_with_background(t, N0, lamb, back):
-    return N0 * unp.exp(-lamb*t) + back
+    return N0 * unp.exp(-lamb * t) + back
 
-N = np.genfromtxt("./muon_data.txt")
+
+N = np.genfromtxt("data/muon_data.txt")
 N = N[5:-5]
 N = N[::2]
-t = (2*np.arange(len(N)) + 5)/21.48
+t = (2 * np.arange(len(N)) + 5) / 21.48
 N_err = np.sqrt(N)
 
 
 params, cov = curve_fit(decay_with_background, t, N, p0=[400, 1, 5])
-
 N0, Lamb, U = correlated_values(params, cov)
 
 print("N0=", N0)
@@ -38,21 +35,20 @@ print("τ=", 1 / Lamb)
 t_plot = np.linspace(0, 11, 1000)
 y_plot = u_decay_with_background(t_plot, N0, Lamb, U)
 
-fig = plt.figure(figsize=(15/2.54, 10/2.54))
+fig = plt.figure(figsize=(15 / 2.54, 10 / 2.54))
 ax = fig.add_subplot(1, 1, 1)
 
 data = ax.errorbar(
     t,
     N,
     yerr=N_err,
-    fmt='k+',
+    xerr=np.diff(t)[0] / 2,
+    color='k',
     label="Messwerte",
-    capsize=1,
-    ms=3,
-    capthick=0.5,
-    elinewidth=0.75,
+    ls='',
+    lw=1,
 )
-fit, = ax.plot(t_plot, nom(y_plot), 'r-', label="Fit", lw=0.5)
+fit, = ax.plot(t_plot, nom(y_plot), 'r-', label="Fit", lw=1)
 
 ax.fill_between(
     t_plot,
@@ -93,9 +89,9 @@ ax.legend(
     ["Messwerte", "Fit", r"$1\sigma$-Umgebung ($\times 10$)"],
     loc="best",
 )
-ax.set_xlabel(r"$t/\si{\micro\second}$")
+ax.set_xlabel(r"$t \mathbin{/} \si{\micro\second}$")
 ax.set_ylabel(r"Zählrate")
 ax.set_xlim(0, 11)
 ax.set_ylim(-50, 500)
 fig.tight_layout()
-fig.savefig("muon_plot.png", dpi=150)
+fig.savefig("build/muon_plot.png", dpi=150)
