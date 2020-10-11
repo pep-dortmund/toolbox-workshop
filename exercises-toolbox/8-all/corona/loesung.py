d363053065cb8e7f2abc7217b6e9e1af436e6737
chrbeckm <christian.beckmann@tu-dortmund.de>
2020-10-11 19:12:09 +0200

    apply changes to genfromtxt

--- exercises-toolbox/8-all/corona/loesung.py
+++ exercises-toolbox/8-all/corona/loesung.py
@@ -1,42 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-Datum, positiver, insgesamt, genesene, Verstorbene, anderer_Verstorbene, aktuell_erkrankte, aktuell_stationaer, aktuell_intensiv, aktuell_beatmete = np.genfromtxt('FB53-Coronafallzahlen.csv', delimiter=';', unpack=True, dtype='U', skip_header=1)
+data = np.genfromtxt(
+    './FB53-Coronafallzahlen.csv',
+    delimiter=';',
+    names=True, # column names from first row,
+    encoding='utf-8-sig', # the file has a BOM,
+    dtype=None,  # guess dtypes (keeps Datum a str and the rest integer),
+    missing_values='-',    # missing value indicator
+)
 
-for x in [positiver, insgesamt, genesene, Verstorbene, anderer_Verstorbene, aktuell_erkrankte, aktuell_stationaer, aktuell_intensiv, aktuell_beatmete]:
-    x[x == '-'] = '0'
-    x[x == ''] = '0'
+spalten = data.dtype.names
 
-plt.bar(Datum, positiver.astype(int), label='Positive Fälle')
+plt.bar(data['Datum'], data['Zuwachs_positiver_Testergebnisse_zum_Vortag'], label='Positive Fälle')
 liste = []
-for i in range(Datum.size - 7):
-    liste.append(np.mean(positiver[i:i+7].astype(int)))
-plt.plot(Datum[7:], liste, 'r.', markersize=3, label='7-Tage Inzidenz')
+for i in range(data['Datum'].size - 7):
+    liste.append(np.mean(data['Zuwachs_positiver_Testergebnisse_zum_Vortag'][i:i+7].astype(int)))
+plt.plot(data['Datum'][7:], liste, 'r.', markersize=3, label='7-Tage Inzidenz')
 plt.legend()
-plt.xticks(Datum[::10], Datum[::10], rotation=90)
-plt.xlim(-1, Datum.size)
+plt.xticks(data['Datum'][::10], data['Datum'][::10], rotation=90)
+plt.xlim(-1, data['Datum'].size)
 plt.tight_layout()
 plt.savefig('loesung-faelle-pro-tag.pdf')
 plt.clf()
 
-plt.bar(Datum, insgesamt.astype(int), label='Fälle gesamt')
-plt.bar(Datum, genesene.astype(int), label='Genesene gesamt')
-plt.bar(Datum, Verstorbene.astype(int), label='Verstorbene gesamt')
-plt.bar(Datum, anderer_Verstorbene.astype(int), label='andere Verstorbene gesamt')
+plt.bar(data['Datum'], data['positive_Testergebnisse_insgesamt'].astype(int), label='Fälle gesamt')
+plt.bar(data['Datum'], data['genesene_Personen_gesamt'].astype(int), label='Genesene gesamt')
+plt.bar(data['Datum'], data['ursächlich_an_COVID19_Verstorbene'].astype(int), label='Verstorbene gesamt')
+plt.bar(data['Datum'], data['aufgrund_anderer_Ursachen_Verstorbene'].astype(int), label='andere Verstorbene gesamt')
 plt.legend()
-plt.xticks(Datum[::10], Datum[::10], rotation=90)
-plt.xlim(-1, Datum.size)
+plt.xticks(data['Datum'][::10], data['Datum'][::10], rotation=90)
+plt.xlim(-1, data['Datum'].size)
 plt.tight_layout()
 plt.savefig('loesung-insgesamt.pdf')
 plt.clf()
 
-plt.bar(Datum, aktuell_erkrankte.astype(int), label='erkrankt')
-plt.bar(Datum, aktuell_stationaer.astype(int), label='stationär')
-plt.bar(Datum, aktuell_intensiv.astype(int), label='intensiv')
-plt.bar(Datum, aktuell_beatmete.astype(int), label='beatmet')
+plt.bar(data['Datum'], data['aktuell_erkrankte_Personen'].astype(int), label='erkrankt')
+plt.bar(data['Datum'], data['darunter_aktuell_stationär_behandelte_Personen'].astype(int), label='stationär')
+plt.bar(data['Datum'], data['darunter_aktuell_intensivmedizinisch_behandelte_Personen'].astype(int), label='intensiv')
+plt.bar(data['Datum'], data['darunter_aktuell_beatmete_Personen'].astype(int), label='beatmet')
 plt.legend()
-plt.xticks(Datum[::10], Datum[::10], rotation=90)
-plt.xlim(-1, Datum.size)
+plt.xticks(data['Datum'][::10], data['Datum'][::10], rotation=90)
+plt.xlim(-1, data['Datum'].size)
 plt.tight_layout()
 plt.savefig('loesung-aktuell.pdf')
 plt.clf()
