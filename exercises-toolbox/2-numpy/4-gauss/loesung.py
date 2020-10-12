562faec00dd056fb5437652e3ad93240f7a790cb
chrbeckm <christian.beckmann@tu-dortmund.de>
2020-10-12 19:58:21 +0200

    rewording in numpy exercises

--- exercises-toolbox/2-numpy/4-gauss/loesung.py
+++ exercises-toolbox/2-numpy/4-gauss/loesung.py
@@ -1,4 +1,6 @@
 import numpy as np
 
-xs = np.random.normal(1, 2, size=1000)
-print('Summe: ', np.sum(xs))
+x_new = np.random.default_rng().normal(1., 2., 1000)
+x_old = np.random.normal(1, 2, size=1000)
+print('Summe, neu: ', np.sum(x_new))
+print('Summe, alt:  ', np.sum(x_old))
