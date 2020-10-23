a6cbac8235161bfd892a5c7e531bf26c8879a141
chrbeckm <christian.beckmann@tu-dortmund.de>
2020-10-23 12:30:40 +0200

    changed inzidenz calculation, styled up with tex

--- exercises-toolbox/8-all/corona/loesung.py
+++ exercises-toolbox/8-all/corona/loesung.py
@@ -12,32 +12,57 @@ data = np.genfromtxt(
 
 spalten = data.dtype.names
 
-plt.bar(data['Datum'], data['Zuwachs_positiver_Testergebnisse_zum_Vortag'], label='Positive Fälle')
-liste = np.array([])
 dortmund = 6.03609
 rki = 5.88250
-for i in range(data['Datum'].size - 7):
-    liste = np.append(liste, np.sum(data['Zuwachs_positiver_Testergebnisse_zum_Vortag'][i:i+7].astype(int)))
-plt.plot(data['Datum'][7:], liste/dortmund, 'y.', markersize=3, label='7-Tage Inzidenz Do')
-plt.plot(data['Datum'][7:], liste/rki, 'r.', markersize=3, label='7-Tage Inzidenz RKI')
-plt.legend()
+n_positive_tests = np.convolve(data['Zuwachs_positiver_Testergebnisse_zum_Vortag'], np.ones(7))
+
+plt.bar(data['Datum'], data['Zuwachs_positiver_Testergebnisse_zum_Vortag'], label='Positive Fälle')
+
+plt.plot(data['Datum'], n_positive_tests[:-6]/dortmund, 'k.', markersize=3, label='7-Tage Inzidenz Do')
+plt.plot(data['Datum'], n_positive_tests[:-6]/rki, 'c.', markersize=3, label='7-Tage Inzidenz RKI')
+
+plt.plot([-1, data['Datum'].size], [35, 35], '-g', linewidth=0.5)
+plt.plot([-1, data['Datum'].size], [50, 50], '-y', linewidth=0.5)
+plt.plot([-1, data['Datum'].size], [75, 75], '-r', linewidth=0.5)
+
+plt.title('Fälle pro Tag mit 7-Tage-Inzidenz, \n Stadt Dortmund: ' + r'$\num{603609}$ Einwohner, RKI: $\num{588250}$ Einwohner')
+plt.legend(loc='upper center')
 plt.xticks(data['Datum'][::14], data['Datum'][::14], rotation=90)
 plt.xlim(-1, data['Datum'].size)
-plt.tight_layout()
+plt.tight_layout(pad=0)
 plt.savefig('loesung-faelle-pro-tag.pdf')
 plt.clf()
 
-plt.bar(data['Datum'], data['positive_Testergebnisse_insgesamt'].astype(int), label='Fälle gesamt')
-plt.bar(data['Datum'], data['genesene_Personen_gesamt'].astype(int), label='Genesene gesamt')
-plt.bar(data['Datum'], data['ursächlich_an_COVID19_Verstorbene'].astype(int), label='Verstorbene gesamt')
-plt.bar(data['Datum'], data['aufgrund_anderer_Ursachen_Verstorbene'].astype(int), label='andere Verstorbene gesamt')
+plt.plot(data['Datum'], n_positive_tests[:-6]/dortmund, 'k.', markersize=3, label='7-Tage Inzidenz Do')
+plt.plot(data['Datum'], n_positive_tests[:-6]/rki, 'c.', markersize=3, label='7-Tage Inzidenz RKI')
+
+plt.plot([-1, data['Datum'].size], [35, 35], '-g', linewidth=0.5)
+plt.plot([-1, data['Datum'].size], [50, 50], '-y', linewidth=0.5)
+plt.plot([-1, data['Datum'].size], [75, 75], '-r', linewidth=0.5)
+
+plt.title('7-Tage-Inzidenz, \n Stadt Dortmund: ' + r'$\num{603609}$ Einwohner, RKI: $\num{588250}$ Einwohner')
+plt.legend(loc='upper center')
+plt.xticks(data['Datum'][::14], data['Datum'][::14], rotation=90)
+plt.xlim(-1, data['Datum'].size)
+plt.ylim(0, np.max(n_positive_tests/rki)*1.05)
+plt.tight_layout(pad=0)
+plt.savefig('loesung-inzidenz.pdf')
+plt.clf()
+
+plt.title('Gesamtzahl an Fällen')
+plt.bar(data['Datum'], data['positive_Testergebnisse_insgesamt'].astype(int), label='positive Tests')
+plt.bar(data['Datum'], data['genesene_Personen_gesamt'].astype(int), label='Genesene')
+plt.bar(data['Datum'], 50 * data['ursächlich_an_COVID19_Verstorbene'].astype(int), label=r'Verstorbene $\cdot 50$')
+plt.bar(data['Datum'], 50 * data['aufgrund_anderer_Ursachen_Verstorbene'].astype(int), label=r'an anderer Ursache Verstorbene $\cdot 50$')
 plt.legend()
 plt.xticks(data['Datum'][::14], data['Datum'][::14], rotation=90)
 plt.xlim(-1, data['Datum'].size)
-plt.tight_layout()
+plt.ylim(0, np.max(data['positive_Testergebnisse_insgesamt'].astype(int))*1.05)
+plt.tight_layout(pad=0)
 plt.savefig('loesung-insgesamt.pdf')
 plt.clf()
 
+plt.title('Aktuelle Fälle pro Tag')
 plt.bar(data['Datum'], data['aktuell_erkrankte_Personen'].astype(int), label='erkrankt')
 plt.bar(data['Datum'], data['darunter_aktuell_stationär_behandelte_Personen'].astype(int), label='stationär')
 plt.bar(data['Datum'], data['darunter_aktuell_intensivmedizinisch_behandelte_Personen'].astype(int), label='intensiv')
@@ -45,6 +70,7 @@ plt.bar(data['Datum'], data['darunter_aktuell_beatmete_Personen'].astype(int), l
 plt.legend()
 plt.xticks(data['Datum'][::14], data['Datum'][::14], rotation=90)
 plt.xlim(-1, data['Datum'].size)
-plt.tight_layout()
+plt.ylim(0, np.max(data['aktuell_erkrankte_Personen'].astype(int))*1.05)
+plt.tight_layout(pad=0)
 plt.savefig('loesung-aktuell.pdf')
 plt.clf()
