c4bc3bc369bff3bbe89356ce702d8c64ade2b24b
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2021-10-08 14:13:06 +0200

    Use more interesting parameters

--- exercises-latex/12-python/data.py
+++ exercises-latex/12-python/data.py
@@ -5,7 +5,7 @@ rng = np.random.default_rng(42)
 N = 51
 data_x = np.linspace(0, 2 * np.pi, N)
 error_y = rng.lognormal(-1, 0.2, size=N)
-data_y = rng.normal(np.sin(data_x), error_y)
+data_y = rng.normal(2 * np.sin(1.5 * data_x + np.pi / 4) + 0.5, error_y)
 np.savetxt(
     'data.txt',
     np.column_stack([data_x, data_y, error_y]),
