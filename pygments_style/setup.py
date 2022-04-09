bc9de68319a5e5e6903a06d9af3504b797add97a
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2022-04-09 15:38:26 +0200

    Use custom pygments style

---
+++ pygments_style/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name="toobox_style",
+    version="1.0.0",
+    py_modules=["toolbox_style"],
+    entry_points={
+        "pygments.styles": [
+            "toolbox = toolbox_style:Toolbox"
+        ]
+    }
+)
