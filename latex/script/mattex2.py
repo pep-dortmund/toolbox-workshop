6a47d3c5ea83c8183f1f88655a106ed22a8d4a44
Christian Beckmann <christian.beckmann@tu-dortmund.de>
2022-08-01 15:31:53 +0200

    latex slides part 2 2022

--- latex/script/mattex2.py
+++ latex/script/mattex2.py
@@ -12,9 +12,9 @@ import numpy as np
 
 x = np.linspace(0, 10, 1000)
 y = x ** np.sin(x)
-plt.figure(figsize=(6.022, 3.39))                 # <-- set figure size
+# set figure size and use constrained_layout
+plt.figure(figsize=(6.022, 3.39), constrained_layout=True)
 plt.plot(x, y)
 plt.xlabel(r'$\alpha / \Omega$')
 
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)   # <-- use tight_layout
-plt.savefig('build/figures/mattex2.pdf')
+plt.savefig('build/figures/mattex2.pdf', bbox_inches='tight', pad_inches=0)
