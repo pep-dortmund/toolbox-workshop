8d40a080b89b6a37b95f6698f440ecebf81af4b0
chrbeckm <christian.beckmann@tu-dortmund.de>
2022-10-02 13:40:40 +0200

    spell checks and changes

--- exercises-toolbox/2-numpy/1-arrays/loesung.py
+++ exercises-toolbox/2-numpy/1-arrays/loesung.py
@@ -53,9 +53,9 @@ print(f"Array f.reshape(-1, 1):\n{f_2d}")
 
 # Aufgabe 5)
 # Das was hier passiert nennt sich 'numpy-broadcasting' und ist eine sehr 'mächtiges' feature von numpy arrays.
-# Es ist nicht notwendig dieses Feature von Anfang an zu verstehen, es erweißt sich nur irgendwann als
+# Es ist nicht notwendig dieses Feature von Anfang an zu verstehen, es erweist sich nur irgendwann als
 # sehr nützlich.
-# Details findest du in der Dokumentation: https://docs.scipy.org/doc/numpy-1.13.0/user/basics.broadcasting.html
+# Details findest du in der Dokumentation: https://numpy.org/doc/stable/user/basics.broadcasting.html 
 
 print("\nAufgabe 5*)")
 print("Zum Vergleich noch mal die beiden Summanden:")
