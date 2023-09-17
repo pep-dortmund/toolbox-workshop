46712409a3ad70c436e8b8ba37c2b8a977e40716
Christian Beckmann <christian.beckmann@udo.edu>
2023-09-17 18:01:36 +0200

    change second part of 2-numpy/3-filter to x < 0

--- exercises-toolbox/2-numpy/3-filter/loesung.py
+++ exercises-toolbox/2-numpy/3-filter/loesung.py
@@ -2,9 +2,9 @@ import numpy as np
 
 x, y = np.genfromtxt("data.txt", unpack=True)
 
-print(np.sum(x > 0))
+print(f"Es sind {np.sum(x > 0)} von {len(x)} Wertepaaren mit x > 0.")
 
-selected = y[x > 0]
+selected = y[x < 0]
 
 print("Mittelwert: ", np.mean(selected))
 print("Standardabweichung: ", np.std(selected))
