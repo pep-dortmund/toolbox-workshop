c8e4a106df90322b1b693a2a90137c5f3b8b3c73
Sebastian Lütge <sebastian.luetge@tu-dortmund.de>
2024-08-05 16:08:37 +0200

    test for 1-1 works

--- common/generate_vorlage.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-[ ! -f 'vorlage.py.dummy' ] || cp loesung.py vorlage.py
-[ ! -f 'vorlage.py.dummy' ] || find . -name 'vorlage.py' \
-		-exec sed -i -e '/# begin solution/,/# end solution/d' {} \;
-
