c6557e31d09237ba7119410650883084363cdc4a
Sebastian Lütge <sebastian.luetge@tu-dortmund.de>
2024-08-05 14:52:07 +0200

    test for 1-1

--- common/generate_vorlage.sh
+++ common/generate_vorlage.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-find . -name 'vorlage.py' -exec "cp loesung.py vorlage.py \
-		& sed -i -e '/# begin solution/,/# end solution' {} \;"
+[ ! -f 'vorlage.py.dummy' ] || cp loesung.py vorlage.py
+[ ! -f 'vorlage.py.dummy' ] || find . -name 'vorlage.py' \
+		-exec sed -i -e '/# begin solution/,/# end solution/d' {} \;
 
