59a2817f284767c193b6e986aeb311e3c85fbb18
Sebastian Lütge <sebastian.luetge@tu-dortmund.de>
2024-08-06 12:15:57 +0200

    more generalised targets
    trying to make the build process more general and robust

--- common/generate_vorlage.mk
+++ common/generate_vorlage.mk
@@ -1,11 +1,10 @@
 FILES := $(sort $(wildcard */loesung*.py))
+targets := $(subst loesung,vorlage,$(FILES))
 
-define gen_vorlage = 
-	grep -q '# begin solution' $(1) && : >$(dir $1)vorlage.py
-	@[ ! -f "$(dir $1)vorlage.py" ] || cp $(1) $(dir $1)vorlage.py
-	@[ ! -f "$(dir $1)vorlage.py" ] || sed -i -e '/# begin solution/,/# end solution/d' $(dir $1)vorlage.py
-	@[ -s "$(dir $1)vorlage.py" ] || rm "$(dir $1)vorlage.py"
-endef 
 
-all: $(FILES)
-	$(foreach loesung, $(FILES), $(call gen_vorlage,$(loesung));)
+all: $(FILES) $(targets)
+	# $(foreach (loesung,vorlage), ($(FILES), $(targets)), $(call gen_vorlage,$(loesung),$(vorlage));)
+
+$(targets): %: ../../common/gen_vorlage.sh
+	@echo $@
+	bash $< $@
