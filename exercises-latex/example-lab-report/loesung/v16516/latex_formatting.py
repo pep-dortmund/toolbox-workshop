c028a4c182d9b7d956992ef59f093f1fbb15c43e
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2024-09-18 19:56:17 +0200

    add some 'library' files

---
+++ exercises-latex/example-lab-report/loesung/v16516/latex_formatting.py
@@ -0,0 +1,18 @@
+import numpy as np
+from uncertainties.unumpy import (
+    std_devs as stds,
+)
+
+def make_qty(num, unit, exp="", figures=None):
+    """Format an uncertainties ufloat as a \qty quantity"""
+    if np.any(stds([num])):
+        if figures is None:
+            figures = ""
+        x = "{0:.{1:}uf}".format(num, figures).replace("/", "")
+    else:
+        x = "{0:.{1:}f}".format(num, figures)
+
+    return r"\qty{{{}{}}}{{{}}}".format(x, exp, unit)
+
+
+
