5be559143d20e5dd6cf290a82b91d400af0123b3
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2024-09-27 08:46:32 +0200

    changes to script

--- exercises-toolbox/8-all/example-report/generate-step-scripts.py
+++ exercises-toolbox/8-all/example-report/generate-step-scripts.py
@@ -1,15 +1,31 @@
+import argparse
 import re
 import sys
 from collections import defaultdict
 
 STEPRANGEREGEX = re.compile(r"(?:#|%)\s<(\d*)(-(\d*)|)>")
 
+def parse_int_tuple(tuple_str):
+    values = tuple_str.split(",")
+    return (int(values[0]), int(values[1]))
 
 
-def extract_stepranges(lines):
+def setup_arg_parser():
+    parser = argparse.ArgumentParser(
+                    prog="generate-step-scripts",
+                    description="Generate multiple files with lines from a template file",
+                    epilog='')
+
+    parser.add_argument('-v', '--verbose', action='store_true') 
+    parser.add_argument('-r', '--map-from-range',type=parse_int_tuple)
+    parser.add_argument('-t','--template_filepath', dest='template_filepath')           
+    parser.add_argument('-o', '--output_filepaths', dest='output_filepaths', action="append")
+    return parser                    
+
+def extract_stepranges(lines, max_upper_limit):
     step_ranges = []
     script_lines = []
-    for i,l in enumerate(lines):
+    for l in lines:
         found = STEPRANGEREGEX.search(l)
         if found is None: 
             continue
@@ -17,33 +33,23 @@ def extract_stepranges(lines):
 
         lower_limit = int(groups[0])
         if (not groups[1]) and (groups[2] is None):
-             step_range = (lower_limit, lower_limit)
+             step_limits = (lower_limit, lower_limit + 1)
         elif not groups[2]:
-            step_range = (lower_limit, None)
+            step_limits = (lower_limit, max_upper_limit + 1)
         else:
-            step_range = (lower_limit, int(groups[2]))
-        step_ranges.append(step_range)
+            step_limits = (lower_limit, int(groups[2]) + 1)
+        step_ranges.append(range(*step_limits))
         script_lines.append(STEPRANGEREGEX.sub("", l).rstrip())
 
     return step_ranges, script_lines
 
 
-def step_list_from_step_range(step_range, upper_limit):
-    lower_limit = int(step_range[0])
-    if not step_range[1] is None:
-        upper_limit = int(step_range[1])
-    return list(range(lower_limit, upper_limit+1))
-
-
-
-def generate_step_file_lines(template_lines):
-    step_ranges, script_lines = extract_stepranges(template_lines)
-    highest_upper_limit = max([int(sr[1]) for sr in step_ranges if sr[1]]) + 1
-
-    step_lists = [step_list_from_step_range(sr, highest_upper_limit) for sr in step_ranges]
+def generate_step_file_lines(template_lines, num_output_files):
+    step_ranges, script_lines = extract_stepranges(template_lines, num_output_files)
      
     files_lines = defaultdict(list)
-    for sl, line in zip(step_lists, script_lines):
+    for sl, line in zip(step_ranges, script_lines):
+        
         print(f"sl: {sl}: {line}")
         for step in sl:
             files_lines[step].append(line)
@@ -53,27 +59,39 @@ def generate_step_file_lines(template_lines):
 
 
 def main():
-    filepath = sys.argv[1]
-    print(f"Filename: {filepath}")
-    output_filepath = filepath 
-    if len(sys.argv) > 2:
-        output_filepath = sys.argv[2]
+    parser = setup_arg_parser()
+    args = parser.parse_args()
+    template_filepath = args.template_filepath
+    output_filepaths = args.output_filepaths
+    if args.verbose:
+        print(f"Templatefile: {template_filepath}")
+        print(f"Outputfiles: {output_filepaths}")
+        print(f"range: {args.map_from_range}")
 
-    path, ext = output_filepath.rsplit('.') 
-    output_filepath_pattern =  path + "_step-{step}." + ext
-    print(output_filepath, output_filepath_pattern)
-    with open(sys.argv[1]) as fh:
+    
+    
+
+
+    with open(template_filepath) as fh:
         script_lines = fh.readlines()
     
-    files_lines = generate_step_file_lines(script_lines)
+    files_lines = generate_step_file_lines(script_lines, len(output_filepaths))
 
-    for it in files_lines.items():
-        print(it)
-    
-    for step, lines in files_lines.items():
-        with open(output_filepath_pattern.format(step=step), "w") as fh:
-            fh.write("\n".join(lines).strip())
-        
+    if args.verbose:
+        print(f"Found steps: {files_lines.keys()}")
+
+
+    map_step = lambda step: step 
+    if args.map_from_range:
+        map_step = lambda step: step - args.map_from_range[0]
+
+    try:
+        for step, lines in files_lines.items():
+
+            with open(output_filepaths[map_step(step)-1], "w") as fh:
+                fh.write("\n".join(lines).strip())
+    except IndexError: 
+        raise ValueError(f"Expecting {max(files_lines.keys())} output files, but got only {len(output_filepaths)}.") 
 
 if __name__ == "__main__":
     main()
