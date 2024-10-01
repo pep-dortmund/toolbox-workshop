fdac792d14b25933b8d862b8d20f9058040de243
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2024-10-01 11:07:20 +0200

    add dataclass for codeline

--- exercises-toolbox/8-all/example-report/generate-step-scripts.py
+++ exercises-toolbox/8-all/example-report/generate-step-scripts.py
@@ -2,6 +2,19 @@ import argparse
 import re
 import sys
 from collections import defaultdict
+from dataclasses import dataclass
+from collections.abc import Iterable
+
+@dataclass    
+class Templateline:
+    linenumber: int
+    line: str
+    step_range: Iterable
+    
+    def __str__(self):
+        return f"{self.linenumber:>3} {self.line}"
+
+
 
 STEPRANGEREGEX = re.compile(r"(?:#|%)\s<(\d*)(-(\d*)|)>")
 
@@ -14,16 +27,18 @@ def setup_arg_parser():
     parser.add_argument('-v', '--verbose', action='store_true') 
     parser.add_argument('-n', '--dry-run', action='store_true') 
     parser.add_argument('-s', '--start-step', type=int)
-    parser.add_argument('-t','--template_filepath', dest='template_filepath')           
-    parser.add_argument('-o', '--output_filepaths', dest='output_filepaths', action="append")
+    parser.add_argument('-t','--template_filepath', dest='template_filepath', required=True)           
+    parser.add_argument('-o', '--output_filepaths', dest='output_filepaths', action="append", required=True)
     return parser                    
 
-def extract_stepranges(lines, max_upper_limit):
-    step_ranges = []
+def parse_lines_and_stepranges(lines, max_upper_limit):
+    #step_ranges = []
     script_lines = []
-    for l in lines:
+    removed_lines = []
+    for i,l in enumerate(lines, start=1):
         found = STEPRANGEREGEX.search(l)
         if found is None: 
+            removed_lines.append(Templateline(i,l, (None,  None)))
             continue
         groups = found.groups()
 
@@ -34,30 +49,29 @@ def extract_stepranges(lines, max_upper_limit):
             step_limits = (lower_limit, max_upper_limit + 1)
         else:
             step_limits = (lower_limit, int(groups[2]) + 1)
-        step_ranges.append(range(*step_limits))
-        script_lines.append(STEPRANGEREGEX.sub("", l).rstrip())
+        script_lines.append(Templateline(i, STEPRANGEREGEX.sub("", l).rstrip(), range(*step_limits)))
 
-    return step_ranges, script_lines
+    return script_lines, removed_lines
 
 
-def generate_step_file_lines(template_lines, num_output_files):
-    step_ranges, script_lines = extract_stepranges(template_lines, num_output_files)
-
-    files_lines = defaultdict(list)
-    for sl, line in zip(step_ranges, script_lines):
+def generate_step_file_lines(script_lines):
+    step_files_lines = defaultdict(list)
+    for line in script_lines:
          
-        for step in sl:
-            files_lines[step].append(line)
+        for step in line.step_range:
+            step_files_lines[step].append(line)
     
-
-    return files_lines
+    return step_files_lines
 
 
 def main():
     parser = setup_arg_parser()
     args = parser.parse_args()
+
     template_filepath = args.template_filepath
     output_filepaths = args.output_filepaths
+    
+
     if args.verbose:
         print("Input:")
         print(' '.join(sys.argv))
@@ -74,27 +88,31 @@ def main():
 
 
     with open(template_filepath) as fh:
-        script_lines = fh.readlines()
-    
-    files_lines = generate_step_file_lines(script_lines, len(output_filepaths))
+        template_lines = fh.readlines()
+
+    script_lines, removed_lines = parse_lines_and_stepranges(template_lines, len(output_filepaths)) 
+
+    step_files_lines = generate_step_file_lines(script_lines)
 
     if args.verbose:
-        print(f"Found steps: {files_lines.keys()}")
+        print("Removed lines:")
+        print(f"{'\n'.join(str(rl) for rl in removed_lines)}")
+        print(f"Found steps: {step_files_lines.keys()}")
 
     if args.dry_run:
-        for step,lines in files_lines.items():
-            print(f"\nOutputfile '{output_filepaths[map_step(step)-1]}' (Step: {step}) would contain:")
-            print(f"{"\n".join(lines)}")
+        for step,lines in step_files_lines.items():
+            print(f"\nOutputfile '{output_filepaths[map_step(step)-1]}' (Step: {step}) would these lines from the template file:")
+            print(f"{"\n".join(str(l) for l in lines)}")
         return 
 
 
     try:
-        for step, lines in files_lines.items():
+        for step, lines in step_files_lines.items():
 
             with open(output_filepaths[map_step(step)-1], "w") as fh:
-                fh.write("\n".join(lines).strip())
+                fh.write("\n".join(l.line for l in lines).strip())
     except IndexError: 
-        raise ValueError(f"Expecting {max(files_lines.keys())} output files, but got only {len(output_filepaths)}.") 
+        raise ValueError(f"Expecting {max(step_files_lines.keys())} output files, but got only {len(output_filepaths)}.") 
 
 if __name__ == "__main__":
     main()
