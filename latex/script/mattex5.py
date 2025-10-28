3d395a16c4d26715fa1565dca5f5a33c6474249f
chrbeckm <32388722+chrbeckm@users.noreply.github.com>
2025-10-28 16:53:49 +0100

    change mattex5 to fig, ax = plt.subplots

--- latex/script/mattex5.py
+++ latex/script/mattex5.py
@@ -11,7 +11,7 @@ import numpy as np
 x = np.linspace(0, 10, 1000)
 y = x ** np.sin(x)
 
-fig, ax = plt.subplots(1, 1, layout="constrained")
+fig, ax = plt.subplots(layout="constrained")
 ax.plot(x, y)
 ax.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
 
