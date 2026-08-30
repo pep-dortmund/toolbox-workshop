95fa19e69600a985b0f70eaff3ada329ea576146
Christian Beckmann <christian.beckmann@udo.edu>
2026-08-30 21:16:00 +0200

    update python version and pre-commit

--- exercises-toolbox/1-python/4-histogram/loesung.py
+++ exercises-toolbox/1-python/4-histogram/loesung.py
@@ -1,10 +1,10 @@
 # begin solution
-def histogram(nums):
-    for num in nums:
-        if num < 0:
+def histogram(nums):  # define the function and place the incoming data
+    for num in nums:  # iterate over the elements in the list
+        if num < 0:  # Check for negative numbers
             print("X")
         else:
-            print(num * "*")
+            print(num * "*")  # print the appropriate number of stars
 
 
 # end solution
--- exercises-toolbox/1-python/5-readwrite/loesung1.py
+++ exercises-toolbox/1-python/5-readwrite/loesung1.py
@@ -1,11 +1,11 @@
 # begin solution
 import sys
 
-if len(sys.argv) > 1:
-    filename = sys.argv[1]
+if len(sys.argv) > 1:  # check if a document name is given
+    filename = sys.argv[1]  # get the filename
 else:
     filename = "test.txt"
 
-with open(filename, "r") as f:
-    print(f.read())
+with open(filename, "r") as f:  # open the filename in read-mode
+    print(f.read())  # read the file and print it
 # end solution
